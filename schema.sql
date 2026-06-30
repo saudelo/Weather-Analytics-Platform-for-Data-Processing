07ec5c11e30dd9f8d23e4db2ef0b914e9b2f09e9
Suelem Audelo <84360844+saudelo@users.noreply.github.com>
2026-06-30 09:57:42 -0700

    Added sql schema

---
+++ schema.sql
@@ -0,0 +1,22 @@
+CREATE SCHEMA weather_analytics;
+
+CREATE TABLE weather_analytics.city (
+    city_id SERIAL PRIMARY KEY,
+    city VARCHAR(50) NOT NULL,
+    us_state VARCHAR(50) NULL
+    --will clean state using pandas and fill in values if necessary
+);
+
+CREATE TABLE weather_analytics.weather_data (
+    weather_id SERIAL PRIMARY KEY,
+    city_id INT NOT NULL REFERENCES weather_analytics.city (city_id),
+    weather_date DATE NOT NULL,
+    temp_max_f FLOAT NOT NULL,
+    temp_min_f FLOAT NOT NULL,
+    temp_mean_f FLOAT NULL,
+    precip_sum_in FLOAT NOT NULL,
+    precip_hours FLOAT NULL,
+    wind_speed_max_mph FLOAT NOT NULL
+--made some fields nullable since some were optional for this assignment
+--i will clean any missing values using pandas
+);
